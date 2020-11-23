ee9a66cba91a5ea38935e3e5591c86162eb0cdab
AndreVero <andriyveremiyenko@gmail.com>
2020-11-23 22:36:01 +0200

    add glide to dependencies and to di

---
+++ app/src/main/java/com/vero/sibs/di/AppComponent.kt
@@ -0,0 +1,24 @@
+package com.vero.sibs.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.vero.sibs.R
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+object AppComponent {
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_buy_main)
+            .error(R.drawable.ic_buy_main)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
